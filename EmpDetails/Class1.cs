81b305b25eb270c1b43b0f921d7ae0624edc77ba
rajyiddish <rajyiddish@users.noreply.github.com>
2017-12-21 06:10:56 +0000

    Initial commit

---
+++ EmpDetails/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace EmpDetails
+{
+    public class EmpDetailsClass : TableEntity
+    {
+        public EmpDetailsClass()
+        {
+        }
+
+        public EmpDetailsClass(int empId, string company)
+        {
+            this.RowKey = empId.ToString();
+            this.PartitionKey = company;
+        }
+
+        public string EmpName { get; set; }
+        public string ContactNo { get; set; }
+        public string Email { get; set; }
+        public string ProfileImage { get; set; }
+    }
+}
+
